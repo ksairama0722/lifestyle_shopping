7eddc2593e2f8e6ee547ccc746888d525e8aa50a
ksairamarao_190030722 <72547034+ksairama0722@users.noreply.github.com>
2022-04-16 20:45:54 +0530

    Add files via upload

---
+++ lifeStyleProject-master/logout.js
@@ -0,0 +1,14 @@
+var cartProductsForBasket = JSON.parse(localStorage.getItem("cartStorageData"));
+
+document.getElementById("btn").addEventListener("click", function () {
+  console.log("hi");
+  if (cartProductsForBasket.length > 0) {
+    console.log("btn");
+    window.location.href = "basket.html";
+  }
+});
+
+document.getElementById("menButton").addEventListener("click", function () {
+  window.location.href = "menPage.html";
+});
+// this part is done by nikhil
